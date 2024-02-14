6ebf1a0e7944a556aa9188d093cebc58ae713a57
kholmetskii <150633190+kholmetskii@users.noreply.github.com>
2024-02-14 18:22:47 +0000

    description of classes

--- Test.py
+++ Test.py
@@ -2,15 +2,17 @@ class Network:
     def __init__(self):
         self.members = []
         self.relationships = []
+
     def add_member(self, name, age, location):
         member = Node(name, age, location)
         self.members += member
 
-    def add_relationship(self,first_person, second_person):
+    def add_relationship(self, first_person, second_person):
         first_person.friends += second_person
         second_person.friends += first_person
         relationship = Graph(first_person, second_person)
         self.relationships += relationship
+
 
 class Node:
     def __init__(self, name, age, location):
